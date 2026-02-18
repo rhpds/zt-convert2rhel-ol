ce2eb89ad60c83bf91faf5d45d273ec566ec1b83
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2026-02-18 14:11:33 -0500

    setup scripting update

--- runtime-automation/module-01/setup-rhel.sh
+++ runtime-automation/module-01/setup-rhel.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 echo "Starting module called module-01" >> /tmp/progress.log
+
+sudo dnf -y install kernel-core
+sudo grubby --set-default /boot/vmlinuz-`rpm -q --qf "%{BUILDTIME}\t%{EVR}.%{ARCH}\n" kernel-core | sort -nr | head -1 | cut -f2`
+sudo reboot
--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -7,7 +7,3 @@ usermod -aG wheel rhel
 echo "Setup vm control01" > /tmp/progress.log
 
 chmod 666 /tmp/progress.log 
-
-sudo dnf -y install kernel-core
-sudo grubby --set-default /boot/vmlinuz-`rpm -q --qf "%{BUILDTIME}\t%{EVR}.%{ARCH}\n" kernel-core | sort -nr | head -1 | cut -f2`
-sudo reboot
