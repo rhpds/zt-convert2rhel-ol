074f5cbab51b224e1376e8b2f42901777cb30cb2
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2026-02-18 13:40:51 -0500

    setup scripting update

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -8,5 +8,6 @@ echo "Setup vm control01" > /tmp/progress.log
 
 chmod 666 /tmp/progress.log 
 
-#dnf install -y nc
-
+sudo dnf -y install kernel-core
+sudo grubby --set-default /boot/vmlinuz-`rpm -q --qf "%{BUILDTIME}\t%{EVR}.%{ARCH}\n" kernel-core | sort -nr | head -1 | cut -f2`
+sudo reboot
