d07e1329d0456ecf42fb2676af730c7791461370
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2026-08-06 14:41:22 -0400

    modified startup scripting to add repo and gpg

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -7,3 +7,7 @@ usermod -aG wheel rhel
 echo "Setup vm control01" > /tmp/progress.log
 
 chmod 666 /tmp/progress.log 
+
+# add Convert2RHEL repo and GPG key
+curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release https://security.access.redhat.com/data/fd431d51.txt
+curl -o /etc/yum.repos.d/convert2rhel.repo https://cdn-public.redhat.com/content/public/repofiles/convert2rhel-for-rhel-9-x86_64.repo
